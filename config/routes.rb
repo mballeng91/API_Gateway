6b0e63f641355126e8d25ed1a0cc0784ed080636
Helmer Avendaño <helmer.sionica@gmail.com>
2017-09-24 16:28:03 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/sign-in', to: 'authenticate#authUser'
+  post '/sign-out', to: 'authenticate#logout'
+
   post '/events/create', to: 'manage_events#createEvent'
+  get '/events/:event_id/attendance', to: 'manage_events#getEventWithAttendance'
+
   post '/users/create', to: 'manage_users#createUser'
   put '/users/update/:id', to: 'manage_users#updateUser'
+
   post '/attendance/:event_id', to: 'manage_events#defineAttendance'
-  get '/events/:event_id/attendance', to: 'manage_events#getEventWithAttendance'
 end
