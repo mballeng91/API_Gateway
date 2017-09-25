f2a10f7bf36a21e163c2592946a149b1e035bd82
Helmer Avendaño <helmer.sionica@gmail.com>
2017-09-24 23:40:53 -0500

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/sign-in', to: 'authenticate#authUser'
 
-  post '/events/create', to: 'manage_events#createEvent'
+  post '/events/', to: 'manage_events#createEvent'
+  # put '/events/', to: 'manage_events#updateEvent' Implementar
   post '/events/:event_id/invite', to: 'manage_events#inviteUsers'
   get '/events/', to: 'manage_events#getEvents'
   get '/events/:event_id/attendance', to: 'manage_events#getEventWithAttendance'
+  post '/events/:event_id/attendance', to: 'manage_events#defineAttendance'  
 
-  post '/users/create', to: 'manage_users#createUser'
+  post '/users/', to: 'manage_users#createUser'
+  # put '/users/', to: 'manage_users#updateUser'
+  # delete '/users/', to: 'manage_users#deleteteUser'
   put '/users/update/:id', to: 'manage_users#updateUser'
-
-  post '/attendance/:event_id', to: 'manage_events#defineAttendance'
 end
