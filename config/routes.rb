dc3482a8d553573fa69cd0bf76e4159b9b2026ef
Helmer Avendaño <helmer.sionica@gmail.com>
2017-10-15 22:21:17 -0500

    Cambio rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/sign-in', to: 'authenticate#authUser'
 
-  post '/events/create', to: 'manage_events#createEvent'
+  post '/events/', to: 'manage_events#createEvent'
   post '/events/:event_id/invite', to: 'manage_events#inviteUsers'
   get '/events/', to: 'manage_events#getEvents'
   get '/events/:event_id/attendance', to: 'manage_events#getEventWithAttendance'
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get '/sites', to: 'manage_events#getSites'
 
-  post '/users/create', to: 'manage_users#createUser'
+  post '/users/', to: 'manage_users#createUser'
   put '/users/update/:id', to: 'manage_users#updateUser'
   get '/users/search/:email', to: 'manage_users#searchUsers'
 
