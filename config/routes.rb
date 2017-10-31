625c80d5fafdda3b5b0a991ad9019f4289633f08
debeltranc <debeltranc@unal.edu.co>
2017-10-30 20:30:00 -0500

    Revert "Feat/site by id"

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   post '/sign-in', to: 'authenticate#authUser'
   get '/logout', to: 'authenticate#logout'
 
-
   # Event-related endpoints
-
   post '/events/', to: 'manage_events#createEvent'
   post '/events/:event_id/invite', to: 'manage_events#inviteUsers'
   get '/events/', to: 'manage_events#getEvents'
@@ -13,18 +11,15 @@ Rails.application.routes.draw do
   get '/events/myEvents', to: 'manage_events#getEventsByOwnerId'
   get '/events/place/:place_id', to: 'manage_events#getEventsByPlace'
   get '/events/myInvitations', to: 'manage_events#getMyInvitations'
+  put '/events/:event_id/attendance', to: 'manage_events#defineAttendance'
 
+  # Places-related endpoints
+  get '/places', to: 'manage_events#getPlaces'
 
-  get '/sites', to: 'manage_events#getSites'
-  get '/sites/by_id', to: 'manage_events#site_by_id'
-
+  # users-related end
   post '/users/', to: 'manage_users#createUser'
   get '/users/myProfile', to: 'manage_users#profile'
   put '/users/update/:id', to: 'manage_users#updateUser'
   get '/users/search', to: 'manage_users#searchUsers'
-
-
-  put '/events/:event_id/attendance', to: 'manage_events#defineAttendance'
-
 
 end
